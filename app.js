2d8989ecb6ca556fab51bc46c1e45a8be3d35c63
StormKing969 <wijesinghesaj@gmail.com>
2022-01-05 12:42:09 -0500

    update

--- app.js
+++ app.js
@@ -5,71 +5,66 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+// needed to use EJS
 app.set("view engine", "ejs");
 
-// res.send("") => sends one text
-
-
-// res.write("") => used to send multiple texts 
-// res.write("")
-// res.send()
-
-// res.sendFile => sends an entire html file
+// need to use "body"
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
 
 app.get("/", function(req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    let today = new Date();
+    
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    // if(currentDay === 0) {
-    //     day = "Sunday";
-    //     // write a text
-    //     // res.write("<h1>It's a weedend :)</h1>")
-    // } else if (currentDay === 1) {
-    //     day = "Monday";
-    // } else if (currentDay === 2) {
-    //     day = "Tuesday";
-    // } else if (currentDay === 3) {
-    //     day = "Wednesday";
-    // } else if (currentDay === 4) {
-    //     day = "Thursday";
-    // } else if (currentDay === 5) {
-    //     day = "Friday";
-    // } else {
-    //     day = "Saturday";
-    //     // send a file
-    //     // res.sendFile(__dirname + "/index.html")
-    // }
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;     
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;            
-        default:
-            console.log("Error: Current Day value is: " + currentDay); 
-            break;
-    }
+    let day = today.toLocaleDateString("en-US", options);
 
     // Passes the result to list.ejs
-    res.render("list", {kindOfDay: day});
+    res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        // When post, save the value in variable then send to the "get"
+        res.redirect("/");
+    }
+
+    
+});
+
+// ==============================================================================
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res) {
+    let item = req.body.newItem;
+
+    workItems.push(item);
+    
+    // When post, save the value in variable then send to the "get"
+    res.redirect("/work");
+});
+
+
+// ============================================================================
+
+app.get("/about", function(req, res) {
+    res.render("about");
 });
 
 app.listen(3000, function() {
