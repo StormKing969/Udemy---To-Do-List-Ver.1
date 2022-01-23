7d05c9af33609cb0cba69a86765519fcbb4ba1d2
StormKing969 <wijesinghesaj@gmail.com>
2022-01-22 20:23:29 -0500

    update

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-// jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const port = 3000;
 
 // importing local module
 const date = require(__dirname + "/date.js");
 
 const app = express();
-
-let items = [];
-let workItems = [];
 
 // needed to use EJS
 app.set("view engine", "ejs");
@@ -18,6 +15,39 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 // used to style the pages
 app.use(express.static("public"))
+
+let workItems = [];
+
+// Create mongoose DB
+mongoose.connect("mongodb://localhost:27017/ToDoList");
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Wake"
+});
+
+const item2 = new Item({
+    name: "Brush your teeth"
+});
+
+const item3 = new Item({
+    name: "Clean your bed"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(error, docs) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log("Successful")
+    }
+});
 
 app.get("/", function(req, res) {
     let day = date();
@@ -63,6 +93,6 @@ app.get("/about", function(req, res) {
     res.render("about");
 });
 
-app.listen(3000, function() {
-    console.log("Server is connected to port 3000")
+app.listen(port, function() {
+    console.log("Server is connected to port " + port)
 });
