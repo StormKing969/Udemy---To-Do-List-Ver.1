f5053f9dc7ed6d84497f954f4bccef92ca1ac53f
StormKing969 <wijesinghesaj@gmail.com>
2022-02-03 12:59:23 -0500

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // importing local module
 const date = require(__dirname + "/date.js");
